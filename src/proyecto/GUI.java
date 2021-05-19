7d91e2b756acd209e9eda7f9d7199e32e0d44ea2
Santiago <santi.cardona.12@icloud.com>
2021-05-19 08:51:32 -0500

    GUI actualizda, solo falta crear variable arbol y mostrar la respuesta

--- src/proyecto/GUI.java
+++ src/proyecto/GUI.java
@@ -8,8 +8,9 @@ import java.awt.event.ActionListener;
 public class GUI {
 
 	private JFrame frmEd;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField jarra1;
+	private JTextField jarra2;
+	private JTextField objetivo;
 
 	/**
 	 * Launch the application.
@@ -40,7 +41,7 @@ public class GUI {
 	private void initialize() {
 		frmEd = new JFrame();
 		frmEd.setTitle("ED2");
-		frmEd.setBounds(100, 100, 485, 535);
+		frmEd.setBounds(100, 100, 502, 535);
 		frmEd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmEd.getContentPane().setLayout(null);
 		
@@ -49,37 +50,67 @@ public class GUI {
 		frmEd.getContentPane().add(jarra_1_lbl);
 		
 		JLabel jarra_2_lbl = new JLabel("Capacidad jarra 2");
-		jarra_2_lbl.setBounds(186, 12, 111, 16);
+		jarra_2_lbl.setBounds(133, 12, 111, 16);
 		frmEd.getContentPane().add(jarra_2_lbl);
 		
-		textField = new JTextField();
-		textField.setBounds(12, 40, 111, 20);
-		frmEd.getContentPane().add(textField);
-		textField.setColumns(10);
+		jarra1 = new JTextField();
+		jarra1.setBounds(12, 40, 111, 20);
+		frmEd.getContentPane().add(jarra1);
+		jarra1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(186, 40, 111, 20);
-		frmEd.getContentPane().add(textField_1);
+		jarra2 = new JTextField();
+		jarra2.setColumns(10);
+		jarra2.setBounds(133, 39, 111, 20);
+		frmEd.getContentPane().add(jarra2);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		textArea.setEditable(false);
-		textArea.setBounds(12, 140, 445, 344);
-		frmEd.getContentPane().add(textArea);
+		JTextArea res = new JTextArea();
+		res.setLineWrap(true);
+		res.setEditable(false);
+		res.setBounds(12, 140, 461, 344);
+		frmEd.getContentPane().add(res);
 		
 		JButton calcularBtn = new JButton("Calcular");
 		calcularBtn.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textArea.setText("Hola");
+				if(!jarra1.getText().isEmpty() || !jarra2.getText().isEmpty() || !objetivo.getText().isEmpty()) {
+					try {
+						Short jar1 = Short.parseShort(jarra1.getText());
+						Short jar2 = Short.parseShort(jarra2.getText());
+						Short objet = Short.parseShort(objetivo.getText());
+						//TODO Crear arbol y pasar los parametros
+						
+					}catch(NumberFormatException NEx) {
+						JOptionPane.showMessageDialog(null, "Los valores ingresados no son numericos", null, JOptionPane.ERROR_MESSAGE);
+						jarra1.setText(null);
+						jarra2.setText(null);
+						objetivo.setText(null);
+						res.setText(null);
+					}
+				}else {
+					JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", null, JOptionPane.ERROR_MESSAGE);
+					jarra1.setText(null);
+					jarra2.setText(null);
+					objetivo.setText(null);
+					res.setText(null);
+				}
+				
 			}
 		});
-		calcularBtn.setBounds(359, 40, 98, 20);
+		calcularBtn.setBounds(375, 40, 98, 20);
 		frmEd.getContentPane().add(calcularBtn);
 		
 		JLabel resultado_lbl = new JLabel("Resultado");
 		resultado_lbl.setBounds(12, 112, 111, 16);
 		frmEd.getContentPane().add(resultado_lbl);
+		
+		objetivo = new JTextField();
+		objetivo.setColumns(10);
+		objetivo.setBounds(254, 39, 111, 20);
+		frmEd.getContentPane().add(objetivo);
+		
+		JLabel objetivo_lbl = new JLabel("Objetivo");
+		objetivo_lbl.setBounds(254, 12, 111, 16);
+		frmEd.getContentPane().add(objetivo_lbl);
 	}
 }
